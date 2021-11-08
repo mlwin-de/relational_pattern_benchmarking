4164f8857c38941a68b6d1736272102361572194
afshin sadeghi <sadeghi.afshin@gmail.com>
2021-11-08 01:11:33 +0100

    Delete utils directory

--- utils/run_check_data_leak.sh
+++
@@ -1,9 +0,0 @@
-python check_data_leak.py --path ../Datasets/FB15K_H/Inductive   > report_FB15K_H_Inductive.txt
-python check_data_leak.py --path ../Datasets/FB15K_H/Semi-Inductive-CountBased > report_FB15K_H_Semi_Inductive_CountBased.txt
-python check_data_leak.py --path ../Datasets/FB15K_H/Semi-Inductive-HeadOrTailBased  > report_FB15K_H_Semi_Inductive_HeadOrTailBased.txt
-python check_data_leak.py --path ../Datasets/FB15K_H/Transductive > report_FB15K_H_Transductive.txt
-
-python check_data_leak.py --path ../Datasets/WN18_H/Inductive > report_WN18_H_Inductive.txt
-python check_data_leak.py --path ../Datasets/WN18_H/Semi-Inductive-CountBased > report_WN18_H_Inductive-CountBased.txt
-python check_data_leak.py --path ../Datasets/WN18_H/Semi-Inductive-HeadOrTailBased > report_WN18_H_Inductive-HeadOrTailBased.txt
-python check_data_leak.py --path ../Datasets/WN18_H/Transductive > report_WN18_H_Transductive.txt
